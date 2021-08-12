3a209a565ca776d401f26fef84bd74a089f0616b
Ubuntu <azureuser@ng-garden.pdrxx3m24otudljm3vingkpqld.jnbx.internal.cloudapp.net>
2021-08-12 15:30:52 +0000

    added uuid response

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const https = require('https');
 const fs = require('fs');
-
+const cors = require('cors')
+const morgan = require('morgan')
 const app = express()
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.json())
+
 const port = 3117
 const {generateToken,decodeToken,generateUUID} = require('./utils')
 const {insertInto,activateToken} = require('./mongodb')
@@ -29,6 +34,24 @@ app.get('/auth/gen',async(req,res) => {
 	res.send(Buffer.from(`<a href="/auth/activate/${uuid}">Link</a>`))
 
 })
+
+app.post('/auth/gen2',async(req,res) => {
+	// const token = generateToken({name:'effy'})
+	// const info = decodeToken(token)
+	// console.log(generateUUID())
+	console.log(req.body)
+	console.log("asdasd")
+	const uuid= await insertInto(req.body)
+	// res.set('Content-Type', 'text/html');
+	// res.send(Buffer.from(`<a href="/auth/activate/${uuid}">Link</a>`))
+	res.json({
+		status:'success',
+		uuid
+	})
+
+})
+
+
 
 app.get('/auth/activate/:uuid',async(req,res) =>{
 	const uuid = req.params.uuid
